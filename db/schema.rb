a54c05bac720dae70eca8d9abdbec9388cadae34
Sydney Barovsky <sbarovsky@gmail.com>
2020-10-27 21:03:19 -0700

    added uploads as an active admin resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_28_030643) do
+ActiveRecord::Schema.define(version: 2020_10_28_035104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(version: 2020_10_28_030643) do
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
   end
 
+  create_table "uploads", force: :cascade do |t|
+    t.string "filename"
+    t.string "filetype"
+    t.string "filepath"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_uploads_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -42,4 +52,5 @@ ActiveRecord::Schema.define(version: 2020_10_28_030643) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "uploads", "users"
 end
