efe17e171d212cf270bcca7ca838b13f8a57f004
Sydney Barovsky <sbarovsky@gmail.com>
2020-10-27 21:59:32 -0700

    added fake uploads to seed file

--- app/admin/uploads.rb
+++ app/admin/uploads.rb
@@ -1,19 +1,38 @@
-ActiveAdmin.register Upload do
+ActiveAdmin.register Upload, as: "Files" do
+  actions :all, except: :update
   permit_params :filename, :filetype, :filepath, :user_id
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :filename, :filetype, :filepath, :user_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:filename, :filetype, :filepath, :user_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+  # CONTROLLER
+  controller do
+    def create
+    end
+
+    def destroy
+    end
+  end
+
+  # INDEX PAGE
+  index do
+      selectable_column
+      column :id
+      column "Author", :user_id
+      column :filename
+      column :filetype
+      column :filepath
+      column "Uploaded At", :created_at
+  end
+
+  # SHOW PAGE
+  show do
+  end
+
+  # FORM
+  form title: 'UPLOAD A FILE' do |f|
+    f.inputs "FILE" do
+      f.input :filepath, label: "FILEPATH URL"
+      li "UPLOADED @ #{f.object.created_at}" unless f.object.new_record?
+    end
+    f.actions
+  end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,26 @@ User.create!(
   admin: true
 )
 
+2.times do |n|
+  user = User.where(admin: false).first
+  file = Upload.create!(
+    filename: "file_#{n + 1}",
+    filetype: "csv",
+    filepath: "file_#{n + 1}.csv",
+    user_id: user.id
+  )
+end
+
+3.times do |n|
+  user = User.where(admin: true).first
+  file = Upload.create!(
+    filename: "upload_#{n + 1}",
+    filetype: "csv",
+    filepath: "upload_#{n + 1}.csv",
+    user_id: user.id
+  )
+end
+
 if User.all.empty? then
   puts "\tWARNING: YOUR DB CONTAINS NO USERS"
 else
